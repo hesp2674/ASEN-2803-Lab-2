474b58f1933373ebfa8c415e7d732d3cfdf5bfb9
koya6354 <koya6354@colorado.edu>
2024-02-27 20:24:25 -0700

    Plot Experimental vs Model. Something wrong with our model

--- LCSMAIN.m
+++ LCSMAIN.m
@@ -1,16 +1,17 @@
 clear; close all; clc;
 
-r = 0.085; % meters
+r = 0.0085; % meters
 d = 0.155; % meters
 l = 0.255; % meters
-theta = 90; % degrees
-w = 180; % degrees/sec
+theta = 0:360*6; % degrees
+w = 100; % degrees/sec
 
 %Converting to radians for math purposes
-theta = theta*(pi/180);
-w = w*(pi/180);
+%theta = theta*(pi/180);
+%w = w*(pi/180);
 
 v_mod = LCSMODEL(r,d,l,theta,w);
+
 
 %Get experimental value from each test
 [theta_exp_5pt5,w_exp_5pt5,v_exp_5pt5,time_5pt5] = LCSDATA('Test1_5pt5V');
@@ -20,52 +21,63 @@ v_mod = LCSMODEL(r,d,l,theta,w);
 [theta_exp_9pt5,w_exp_9pt5,v_exp_9pt5,time_9pt5] = LCSDATA('Test1_9pt5V');
 [theta_exp_10pt5,w_exp_10pt5,v_exp_10pt5,time_10pt5] = LCSDATA('Test1_10pt5V');
 
+v_mod_5pt5 = LCSMODEL(r,d,l,theta_exp_5pt5,w_exp_5pt5);
+
+figure(1)
+plot(theta_exp_5pt5,v_mod_5pt5);
+xlabel('Theta (deg)');
+ylabel('Velocity (cm/s)');
+
 
 %Plotting velocity vs theta for each test
-figure(1)
+figure(2)
 plot(theta_exp_5pt5,v_exp_5pt5);
-xlabel('Experiment Theta Value');
-ylabel('Experiment Velocity Value');
+xlabel('Experiment Theta Value (deg)');
+ylabel('Experiment Velocity Value (cm/s)');
 title('Velocoity vs Theta Graph for Experiment with 5.5V');
 xlim([0,3600]);
 ylim([-200,225]);
+hold on
+plot(theta_exp_5pt5,v_mod_5pt5);
+legend('Exp','Model');
+hold off
 
-figure(2)
+figure(3)
 plot(theta_exp_6pt5,v_exp_6pt5);
-xlabel('Experiment Theta Value');
-ylabel('Experiment Velocity Value');
+xlabel('Experiment Theta Value (deg)');
+ylabel('Experiment Velocity Value (cm/s)');
 title('Velocoity vs Theta Graph for Experiment with 6.5V');
 xlim([0,3600]);
 ylim([-200,225]);
 
-figure(3)
+figure(4)
 plot(theta_exp_7pt5,v_exp_7pt5);
-xlabel('Experiment Theta Value');
-ylabel('Experiment Velocity Value');
+xlabel('Experiment Theta Value (deg)');
+ylabel('Experiment Velocity Value (cm/s)');
 title('Velocoity vs Theta Graph for Experiment with 7.5V');
 xlim([0,3600]);
 ylim([-200,225]);
 
-figure(4)
+figure(5)
 plot(theta_exp_8pt5,v_exp_8pt5);
-xlabel('Experiment Theta Value');
-ylabel('Experiment Velocity Value');
+xlabel('Experiment Theta Value (deg)');
+ylabel('Experiment Velocity Value (cm/s)');
 title('Velocoity vs Theta Graph for Experiment with 8.5V');
 xlim([0,3600]);
 ylim([-200,225]);
 
-figure(5)
+figure(6)
 plot(theta_exp_9pt5,v_exp_9pt5);
-xlabel('Experiment Theta Value');
-ylabel('Experiment Velocity Value');
+xlabel('Experiment Theta Value (deg)');
+ylabel('Experiment Velocity Value (cm/s)');
 title('Velocoity vs Theta Graph for Experiment with 9.5V');
 xlim([0,3600]);
 ylim([-200,225]);
 
-figure(6)
+figure(7)
 plot(theta_exp_10pt5,v_exp_10pt5);
-xlabel('Experiment Theta Value');
-ylabel('Experiment Velocity Value');
+xlabel('Experiment Theta Value (deg)');
+ylabel('Experiment Velocity Value (cm/s)');
 title('Velocoity vs Theta Graph for Experiment with 10.5V');
 xlim([0,3600]);
 ylim([-200,225]);
--- LCSMODEL.m
+++ LCSMODEL.m
@@ -1,7 +1,11 @@
 function [v_mod] = LCSMODEL(r,d,l,theta,w)
 
 
-beta = asin((d-r*cos(theta))/l);
+%beta = asin((d-r*cos(theta))/l);
 
 
-v_mod = w*r*cos(theta)-(w/l)*r*sin(theta)*tan(beta);
+%v_mod = w*r*cos(theta)-(w/l)*r*sin(theta)*tan(beta);
+
+beta = asind((d-r.*cosd(theta))./l);
+v_mod = -w.*r.*cosd(theta)-(w./l).*r.*sind(theta).*tand(beta);
+
