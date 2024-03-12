7a2f43e0b8569de4c8a8d04a6c1142414cd5f767
hesp2674 <hesp2674@colorado.edu>
2024-03-12 09:12:37 -0600

    Graph Cosmetics

--- LCSMAIN.m
+++ LCSMAIN.m
@@ -31,111 +31,111 @@ v_mod_10pt5 = LCSMODEL(r,d,l,theta_exp_10pt5,w_exp_10pt5);
 
 
 figure(1)
-plot(theta_exp_5pt5,v_mod_5pt5);
+plot(theta_exp_5pt5,v_mod_5pt5, "-o", "LineWidth", 1, MarkerFaceColor="blue", MarkerSize=2);
 xlabel('Theta (deg)');
 ylabel('Velocity (cm/s)');
 
 
 %Plotting velocity vs theta for each test
 figure(2)
-plot(theta_exp_5pt5,v_exp_5pt5);
+plot(theta_exp_5pt5,v_exp_5pt5, "-o", "LineWidth", 1, MarkerFaceColor="blue", MarkerSize=2);
 xlabel('Experiment Theta Value (deg)');
 ylabel('Experiment Velocity Value (cm/s)');
 title('Velocoity vs Theta Graph for Experiment with 5.5V');
 xlim([0,3600]);
 ylim([-200,300]);
 hold on
-plot(theta_exp_5pt5,v_mod_5pt5);
+plot(theta_exp_5pt5,v_mod_5pt5, "-o", "LineWidth", 1, MarkerFaceColor="red", MarkerSize=2);
 legend('Exp','Model');
 hold off
 
 figure(3)
-plot(theta_exp_6pt5,v_exp_6pt5);
+plot(theta_exp_6pt5,v_exp_6pt5, "-o", "LineWidth", 1, MarkerFaceColor="blue", MarkerSize=2);
 xlabel('Experiment Theta Value (deg)');
 ylabel('Experiment Velocity Value (cm/s)');
 title('Velocoity vs Theta Graph for Experiment with 6.5V');
 xlim([0,3600]);
 ylim([-200,300]);
 hold on
-plot(theta_exp_6pt5,v_mod_6pt5);
+plot(theta_exp_6pt5,v_mod_6pt5, "-o", "LineWidth", 1, MarkerFaceColor="red", MarkerSize=2);
 legend('Exp','Model');
 
 figure(4)
-plot(theta_exp_7pt5,v_exp_7pt5);
+plot(theta_exp_7pt5,v_exp_7pt5, "-o", "LineWidth", 1, MarkerFaceColor="blue", MarkerSize=2);
 xlabel('Experiment Theta Value (deg)');
 ylabel('Experiment Velocity Value (cm/s)');
 title('Velocoity vs Theta Graph for Experiment with 7.5V');
 xlim([0,3600]);
 ylim([-200,300]);
 hold on
-plot(theta_exp_7pt5,v_mod_7pt5);
+plot(theta_exp_7pt5,v_mod_7pt5, "-o", "LineWidth", 1, MarkerFaceColor="red", MarkerSize=2);
 legend('Exp','Model');
 
 figure(5)
-plot(theta_exp_8pt5,v_exp_8pt5);
+plot(theta_exp_8pt5,v_exp_8pt5, "-o", "LineWidth", 1, MarkerFaceColor="blue", MarkerSize=2);
 xlabel('Experiment Theta Value (deg)');
 ylabel('Experiment Velocity Value (cm/s)');
 title('Velocoity vs Theta Graph for Experiment with 8.5V');
 xlim([0,3600]);
 ylim([-200,300]);
 hold on
-plot(theta_exp_8pt5,v_mod_8pt5);
+plot(theta_exp_8pt5,v_mod_8pt5, "-o", "LineWidth", 1, MarkerFaceColor="red", MarkerSize=2);
 legend('Exp','Model');
 
 figure(6)
-plot(theta_exp_9pt5,v_exp_9pt5);
+plot(theta_exp_9pt5,v_exp_9pt5, "-o", "LineWidth", 1, MarkerFaceColor="blue", MarkerSize=2);
 xlabel('Experiment Theta Value (deg)');
 ylabel('Experiment Velocity Value (cm/s)');
 title('Velocoity vs Theta Graph for Experiment with 9.5V');
 xlim([0,3600]);
 ylim([-200,300]);
 hold on
-plot(theta_exp_9pt5,v_mod_9pt5);
+plot(theta_exp_9pt5,v_mod_9pt5, "-o", "LineWidth", 1, MarkerFaceColor="red", MarkerSize=2);
 legend('Exp','Model');
 
 figure(7)
-plot(theta_exp_10pt5,v_exp_10pt5);
+plot(theta_exp_10pt5,v_exp_10pt5, "-o", "LineWidth", 1, MarkerFaceColor="blue", MarkerSize=2);
 xlabel('Experiment Theta Value (deg)');
 ylabel('Experiment Velocity Value (cm/s)');
 title('Velocoity vs Theta Graph for Experiment with 10.5V');
 xlim([0,3600]);
 ylim([-200,300]);
 hold on
-plot(theta_exp_10pt5,v_mod_10pt5);
+plot(theta_exp_10pt5,v_mod_10pt5, "-o", "LineWidth", 1, MarkerFaceColor="red", MarkerSize=2);
 legend('Exp','Model');
 
 figure(8)
-plot(time_5pt5,(v_exp_5pt5-v_mod_5pt5));
+plot(time_5pt5,(v_exp_5pt5-v_mod_5pt5), "-o", "LineWidth", 1, MarkerFaceColor="blue", MarkerSize=2);
 xlabel('Time (s)');
 ylabel('Residuals (cm/s)');
 title('Residual vs Time for 5.5V');
 
 figure(9)
-plot(time_6pt5,(v_exp_6pt5-v_mod_6pt5));
+plot(time_6pt5,(v_exp_6pt5-v_mod_6pt5), "-o", "LineWidth", 1, MarkerFaceColor="blue", MarkerSize=2);
 xlabel('Time (s)');
 ylabel('Residuals (cm/s)');
 title('Residual vs Time for 6.5V');
 
 figure(10)
-plot(time_7pt5,(v_exp_7pt5-v_mod_7pt5));
+plot(time_7pt5,(v_exp_7pt5-v_mod_7pt5), "-o", "LineWidth", 1, MarkerFaceColor="blue", MarkerSize=2);
 xlabel('Time (s)');
 ylabel('Residuals (cm/s)');
 title('Residual vs Time for 7.5V');
 
 figure(11)
-plot(time_8pt5,(v_exp_8pt5-v_mod_8pt5));
+plot(time_8pt5,(v_exp_8pt5-v_mod_8pt5), "-o", "LineWidth", 1, MarkerFaceColor="blue", MarkerSize=2);
 xlabel('Time (s)');
 ylabel('Residuals (cm/s)');
 title('Residual vs Time for 8.5V');
 
 figure(12)
-plot(time_9pt5,(v_exp_9pt5-v_mod_9pt5));
+plot(time_9pt5,(v_exp_9pt5-v_mod_9pt5), "-o", "LineWidth", 1, MarkerFaceColor="blue", MarkerSize=2);
 xlabel('Time (s)');
 ylabel('Residuals (cm/s)');
 title('Residual vs Time for 9.5V');
 
 figure(13)
-plot(time_10pt5,(v_exp_10pt5-v_mod_10pt5));
+plot(time_10pt5,(v_exp_10pt5-v_mod_10pt5), "-o", "LineWidth", 1, MarkerFaceColor="blue", MarkerSize=2);
 xlabel('Time (s)');
 ylabel('Residuals (cm/s)');
 title('Residual vs Time for 10.5V');
